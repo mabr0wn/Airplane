2617a255b645102637d528c8e9a060d13045a5bd
Matthew Brown <mattd429@gmail.com>
2017-07-28 15:52:06 -0400

    Create Flight.java

---
+++ Flight.java
@@ -0,0 +1,9 @@
+public class Flight {
+          int seats = 150, passengers;
+          int totalCheckedBags;
+          int maxCarryOns = seats * 2, totalCarryOns;
+  private int flightNumber;
+  private char flightClass;
+  
+  
+}
