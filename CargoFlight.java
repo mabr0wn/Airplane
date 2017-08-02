f40b913b31120c248659362857371ad72d0444ed
Matthew Brown <mattd429@gmail.com>
2017-08-02 15:21:06 -0400

    Update CargoFlight.java

--- CargoFlight.java
+++ CargoFlight.java
@@ -6,7 +6,43 @@ public class CargoFlight extends Flight {
   float maxCargoSpace = 1000.0f;
   float usedCargoSpace;
   
-  public CargoFlight(int flightNumber) {
+ CargoFlight(int flightNumber) {
     super(flightNumber);
+   
   }
+ 
+ CargoFlight(int flightNumber, float maxCargoSpace) {
+  this(flightNumber);
+  this.maxCargoSpace = maxCargoSpace;
+ }
+ 
+ CargoFlight() {
+ 
+ }
+ 
+ Cargoflight(float maxCargoSpace) {
+  this.maxCargoSpace = maxCargoSpace;
+ }
+ 
+ //method
+
+ void add1Package(float h, float w, float d){
+  double size = h * w * d;
+  if(hasCargoSpace((float) size))
+    usedCargoSpace += size;
+  else
+    handleNoSpace();
+ }
+ 
+ private boolean hasCargoSpace(float size) {
+  return usedCargoSpace + size <= maxCargoSpace //1000.0f
+   }
+ private void handleNoSpace() {
+  System.out.println("Not Enough Space");
+ }
+ 
+ @Override
+ int getSeats() { return 12; }
+ 
+ 
 }
