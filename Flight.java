e143a74bb22b5d14dc657d4ba80a465df222508e
Matthew Brown <mattd429@gmail.com>
2017-08-02 16:03:54 -0400

    Update Flight.java

--- Flight.java
+++ Flight.java
@@ -1,11 +1,21 @@
 public class Flight {
-  private int flightNumber;
-  private char flightClass;
-  public  int seats = 150, passengers;
-  public  int totalCheckedBags;
-  public  int maxCarryOns = seats * 2, totalCarryOns;
+  static final int MAX_FAA_SEATS = 550;
+  private int passengers, seats = 150;
+  private Integer flightNumber;
+  private Character flightClass;
+  private boolean[] isSeatsAvailable;
+  int totalCheckedBags;
+  int maxCarryOns = seats * 2, totalCarryOns;
+  //Initializer
+  {
+    isSeatsAvailable = new boolean[seats];
+    for (int i = 0; i < seats; i++)
+      isSeatsAvailable[i] = true
+  }
+  
+  flight() { }
 
-  public flight(int flightNumber) {
+  flight(int flightNumber) {
           this.flightNumber = flightNumber;
   }
   
