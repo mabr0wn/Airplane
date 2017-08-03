00290ad8a3ca1e509b6b05b3c2250232169f0b9a
Matthew Brown <mattd429@gmail.com>
2017-08-03 11:04:54 -0400

    Create FullLicensePilot.java

---
+++ FullLicensePilot.java
@@ -0,0 +1,9 @@
+package com.pluralsight.airplane;
+
+/**
+  *Added to Github on 8/3/17
+  */
+public class FullLicensePilot extends Pilot {
+  @Override
+  boolean canAccept(Flight f) { return true; }
+}  
