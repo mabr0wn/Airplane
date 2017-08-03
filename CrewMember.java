b591e3420bf1abe8b9f63565ec206a57b309dd15
Matthew Brown <mattd429@gmail.com>
2017-08-03 11:10:29 -0400

    Create CrewMember.java

---
+++ CrewMember.java
@@ -0,0 +1,12 @@
+package com.pluralsight.airplane;
+
+/**
+  *Added to github 8/3
+  */
+public class CrewMember {
+  private FlightCrewJob job;
+  
+  CrewMember(FlightCrewJob job) { this.job = job; }
+  void setJob(FlightCrewJob job) { this.job = job; }
+  
+  }
