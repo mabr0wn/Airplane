7c8fed500415afad7be9f84c214a6bbf7ace1cc3
Matthew Brown <mattd429@gmail.com>
2017-08-09 13:23:12 -0400

    create class Person to communicate with Iterface
    simple String name applied.

---
+++ Person.java
@@ -0,0 +1,16 @@
+package com.pluralsight.airplane;
+
+/**
+  *Added to gituhub on 8/9/17
+  */
+public class Person {
+  //each person will at lease have a name, create a data type String variable called name.
+  private String name;
+  
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) { this.name = name; }
+
+}
