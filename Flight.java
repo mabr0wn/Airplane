d46d8496d0c45e7dda1696e90446c1f713da103e
Matthew Brown <mattd429@gmail.com>
2017-08-11 14:26:45 -0400

    updated FlightIterator Class
    Cancelled the Iterator class first made it a nested class moved it into the Flight class then i went ahead and made a anon class since we will not be using this through out our entire program.

--- Flight.java
+++ Flight.java
@@ -7,9 +7,12 @@ import java.util.Iterator;
   *Added to github 8/9/17
   */
 
-public class Flight implements Comparable<Flight>, Iterator<Person> {
+public class Flight implements Comparable<Flight>{
   //set static final to int which will not ever change
   static final int MAX_FAA_SEATS = 550;
+  //like an easy way to keep track of passengers on all flights.
+  //marking as static now means it is not associated with any instance
+  static int allPassengers;
   private int passengers, seats = 150;
   //add wrapper class for null
   private Integer flightNumber;
@@ -32,11 +35,38 @@ public class Flight implements Comparable<Flight>, Iterator<Person> {
     //if positive it will be sorted after
     return flightTime - f.flightTime;
   }
+  //we are going to use a Anonymous class because we are not going to use this through our entire code.
   //iterator is also an interface and has two methods on it
   public Iterator<Person> iterator() {
+    //moving our FlightIterator class inside Flight Class which makes it Inner class but now we will change it to Anon class
     //now we can return back that instance back after creating that class below.
-    return new FlightIterator(crew, roster);
+    return new FlightIterator();
+       //We will need our index field to keep track of our location in the array
+       private int index = 0;
+       //then implment our hasNext() method -- each time throwing back a person
+   
+       @Override
+       public Person hasNext() {
+                //as long as our index is less than the entire crew array and roster array there must be more to come.
+                //inside of here we are going to check where the index is
+                //you can still point to crew and roster even thought they are not apart of FlightIterator they apart of Flight class
+                return index < (crew.length + roster.length);
+       }
+   
+       @Override
+       public Person Next() {
+                //then our next is going to return each indivual person
+                //first we will walk through the crew array then the roster array
+                //if the index is less than the crew length, we will return the crew memeber that resolves to that index
+                Person p = (index < crew.length) ?
+                  //here what we are doing is give me the crew index if there are three then it will show 3 crew members.
+                  //otherwise minus index from crew.length which will set up back to zero for the start of roster index.
+                  crew[index] : roster[index - crew.length];
+                index++;//keep adding until end of index is reached, keep adding back that person.
+                return p;
+         //with this inside our Flight class we can now remove the FlightIterator class
   }
+};   
   //Initializer
   //if seats are avialable add(loop) seats until it reached 150.
   {
@@ -77,18 +107,12 @@ public class Flight implements Comparable<Flight>, Iterator<Person> {
   //            passengers += 1;
   //    }
  
-  //Methods
-  @Override
-  public boolean hasNext() {
-      return false;
-  }
-  
-  @Override
-  public Person next() {
-      return null;
-  }
-  
-  //Return Passengers
+ //Methods
+ //reset all passengers to zeo
+ static void resetAllPassengers() { allPassengers = 0; }
+
+ 
+ //Return Passengers
   private int passengers() { return passengers; }
   
  //Add Ellipse list of passenger(...) since we do not know how many passengers will board plane
@@ -142,6 +166,9 @@ public class Flight implements Comparable<Flight>, Iterator<Person> {
   private void handleToMany() { System.out.println("Too Much Space"); }
   
   //Accessors && Mutators
+  static int getAllPassengers() { return getAllPassengers(); }
+  
+  public void setFlightTime(int flightTime) { this.flightTime = flightTime; }
   
   int getPassengers() { return passengers; }
 
